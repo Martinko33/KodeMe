6e08be0000d378bf571281af51e154ba983bdecd
Martin <martin.lalak@lapiscine.pro>
2021-05-11 15:20:22 +0200

    server test

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -6,12 +6,14 @@ namespace App\Controller\Admin;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/admin")
+ * @IsGranted("ROLE_ADMIN")
  */
 class UserController extends AbstractController
 {
